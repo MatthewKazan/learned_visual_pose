86b6f5e7a96e50f161861d110301afda68236a97
Matthew Kazan <Mattbkazan@icloud.com>
2026-08-07 13:26:29 -0400

    gitignore

---
+++ cpp/app/main.cpp
@@ -0,0 +1,8 @@
+#include <Eigen/Core>
+#include <iostream>
+
+int main()
+{
+	Eigen::Vector3d v(1, 2, 3);
+	std::cout << v << std::endl;
+}
---
+++ cpp/tests/test_sanity.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(SanityTest, ArithmeticWorks) {
+    EXPECT_EQ(2 + 2, 4);
+}
